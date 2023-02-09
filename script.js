3a5d65aab5b08ac3bf34f52c30030e6cb75297b4
Felipe <pipedesings@gmail.com>
2023-02-09 05:18:48 -0500

    Terminacion de pagina de inicio y comienzo de sobre mi

--- script.js
+++ script.js
@@ -28,25 +28,49 @@ function aparecer() {
     video.style.display = `block`;
 }
 
-// const juegoAhorcado = document.querySelector(".juego-ahorcado");
-// const infoPortafolio = document.getElementById("info__portafolio");
-// const infoAhorcado = document.getElementById("info__ahorcado");
-// const infoEncriptador = document.getElementById("info__encriptador");
+const juegoAhorcado = document.querySelector(".juego-ahorcado");
+const infoPortafolio = document.getElementById("info__portafolio");
+const infoAhorcado = document.getElementById("info__ahorcado");
+const infoEncriptador = document.getElementById("info__encriptador");
+
+function cerrar() {
+    infoAhorcado.classList.toggle("info__mostrar");
+}
+
+function abrirAhorcado() {
+    infoAhorcado.classList.toggle("info__mostrar");
+}
+
+function abrirPortafolio() {
+    infoPortafolio.style.display = "flex";
+}
+
+function abrirEncriptador() {
+    infoEncriptador.style.display = "flex";
+}
 
 
-// function cerrar() {
-//     infoEncriptador.style.display = "";
-// }
 
-// function abrirAhorcado() {
-//     infoAhorcado.style.display = "flex";
-// }
+// scripts de slider portafolio
 
-// function abrirPortafolio() {
-//     infoPortafolio.style.display = "flex";
-// }
-
-// function abrirEncriptador() {
-//     infoEncriptador.style.display = "flex";
-// }
-
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    autoplay: {
+        delay: 5000,
+        disbaleOnInteraction: false,
+    },
+    loop: true,
+    speed: 1500,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
